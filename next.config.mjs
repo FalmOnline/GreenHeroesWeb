92c0238e4ee2c833538455cd0813fcd7640dbf9a
Falm <digital@falm.ro>
2024-09-11 19:11:04 +0300

    changing directing links

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
           {
             protocol: 'https',
-            hostname: 'greenheroes.falm.ro',
+            hostname: 'admin.green-heroes.info',
             port: '',
             pathname: '/**',
           },

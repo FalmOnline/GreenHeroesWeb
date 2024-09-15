2ccd93876f073652d2fa121d05ddb75d7b1f1d10
Falm <digital@falm.ro>
2024-09-15 17:08:57 +0300

    mici schimbari la cum este afisat articolul si css

--- app/blog/[postSlug]/page.js
+++ app/blog/[postSlug]/page.js
@@ -23,7 +23,7 @@ export default async function Post({ params }) {
     <div className="mb-12">
       <article>
       <section 
-          className="hero-area h-[60vh] min-h-[30rem] bg-cover bg-center flex items-center relative mb-12 md:container lg:max-w-screen-lg 2xl:max-w-screen-xl 3xl:max-w-screen-2xl mx-auto flex-col justify-center p-2 xs:p-8" style={{ backgroundImage: `url(${imageUrl})` }}>
+          className="hero-area h-[60vh] min-h-[30rem] bg-cover bg-center flex items-center relative mb-12 flex-col justify-center p-2 xs:p-8" style={{ backgroundImage: `url(${imageUrl})` }}>
              <div className="absolute inset-0 bg-black bg-opacity-50"></div>
               <div className="lg:max-w-screen-lg 2xl:max-w-screen-xl 3xl:max-w-screen-2xl mx-auto relative z-10">
                 <h1 className="text-white">{title}</h1>
@@ -35,7 +35,7 @@ export default async function Post({ params }) {
           <section className="md:container lg:max-w-screen-lg 2xl:max-w-screen-xl 3xl:max-w-screen-2xl mx-auto flex flex-col-reverse lg:flex-row px-2 lg:px-4 xl:px-0">
 
 
-          <div dangerouslySetInnerHTML={{ __html: excerpt }} />
+          {/* <div dangerouslySetInnerHTML={{ __html: excerpt }} /> */}
           <div dangerouslySetInnerHTML={{ __html: content }} />
         </section>
       </article>

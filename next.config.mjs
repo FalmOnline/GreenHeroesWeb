edf49ce4128810bec959748b941b63b3637da991
Falm <digital@falm.ro>
2025-04-06 21:56:18 +0300

    improved download buttons

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,39 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//           {
+//             protocol: 'https',
+//             hostname: 'greenheroes.falm.ro',
+//             port: '',
+//             pathname: '/**',
+//           },
+//         ],
+//       },
+// };
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'greenheroes.falm.ro',
-            port: '',
-            pathname: '/**',
-          },
-        ],
-      },
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'greenheroes.falm.ro',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'greengeneration.falm.ro', // Add the second domain here
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
 };
 
 export default nextConfig;
